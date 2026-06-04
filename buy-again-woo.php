6914cf3ff73999c583d3ae56de03d94c052129ae
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2026-06-04 19:48:43 -0300

    feat: test buy again button insert

--- buy-again-woo.php
+++ buy-again-woo.php
@@ -1,12 +1,27 @@
 <?php
 /**
- * Plugin Name: Buy Again Woo
+ * Plugin Name: Buy Again for WooCommerce
  * Description: Buy Again functionality for WooCommerce.
  * Version: 1.0.0
- * Author: Marie Camara
+ * Author: Marilia Camara
  * Text Domain: buy-again-woo
  */
 
 if (!defined('ABSPATH')) {
     exit;
 }
+
+add_filter(
+    'woocommerce_my_account_my_orders_actions',
+    function ($actions, $order) {
+
+        $actions['buy_again'] = [
+            'url'  => '#',
+            'name' => __('Comprar Novamente', 'buy-again-woo'),
+        ];
+
+        return $actions;
+    },
+    10,
+    2
+);
