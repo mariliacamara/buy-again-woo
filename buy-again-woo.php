031e642a1eded010062c647429107b34d136d28b
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2026-06-04 21:05:45 -0300

    style: match buy again button design

--- buy-again-woo.php
+++ buy-again-woo.php
@@ -13,3 +13,17 @@ if (!defined('ABSPATH')) {
 require_once plugin_dir_path(__FILE__) . 'includes/class-buy-again.php';
 
 new Buy_Again();
+
+add_action('wp_enqueue_scripts', function () {
+
+    if (!is_account_page()) {
+        return;
+    }
+
+    wp_enqueue_style(
+        'buy-again-woo',
+        plugin_dir_url(__FILE__) . 'assets/css/buy-again.css',
+        [],
+        '1.0.0'
+    );
+});
