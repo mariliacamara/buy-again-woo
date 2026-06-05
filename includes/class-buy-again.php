5088837ccb22266a5e67c1ec44d625c1d77ece10
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2026-06-04 23:48:26 -0300

    chore: add wordpress and woocommerce phpstan stubs

--- includes/class-buy-again.php
+++ includes/class-buy-again.php
@@ -112,7 +112,12 @@ class Buy_Again {
 		$added  = 0;
 		$failed = array();
 
+    /** @var WC_Order_Item_Product $item */
 		foreach ( $order->get_items() as $item ) {
+
+      if ( ! $item instanceof WC_Order_Item_Product ) {
+        continue;
+      }
 
 			$product_id   = $item->get_product_id();
 			$variation_id = $item->get_variation_id();
@@ -128,11 +133,10 @@ class Buy_Again {
 				$variation_product = wc_get_product( $variation_id );
 
 				if (
-					$variation_product &&
-					$variation_product->is_type( 'variation' )
-				) {
-					$variation = $variation_product->get_variation_attributes();
-				}
+          $variation_product instanceof WC_Product_Variation
+        ) {
+          $variation = $variation_product->get_variation_attributes();
+        }
 			}
 
 			$result = WC()->cart->add_to_cart(
