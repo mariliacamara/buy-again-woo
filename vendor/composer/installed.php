7563c034f2a2496a3ffddda8c97460f948ffe4a5
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2026-06-05 00:25:35 -0300

    chore: include vendor in repository

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'marilia/buy-again-woo',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b744632490640038b17f6bc88ed8af8af9e24477',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'marilia/buy-again-woo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b744632490640038b17f6bc88ed8af8af9e24477',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
